e59bef1a3c296f452f38c65381a00ccf8e01a85e
Anatoli Karpov <karpik-ne@yandex.ru>
2025-01-03 08:53:49 +0100

    start

--- errors/existerror.js
+++
@@ -1,8 +0,0 @@
-class ExistError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 409;
-  }
-}
-
-module.exports = ExistError;
--- middlewares/rateLimit.js
+++
@@ -1,8 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-});
-
-module.exports = rateLimiter;
